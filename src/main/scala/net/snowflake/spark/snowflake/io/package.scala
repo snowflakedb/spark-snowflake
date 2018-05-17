a434aa2fca4d6308bf6b54259657e2e1142c042a
Bing Li <binglihub@gmail.com>
2018-05-17 14:38:51 -0700

    support s3 internal stage with csv format

--- src/main/scala/net/snowflake/spark/snowflake/io/package.scala
+++ src/main/scala/net/snowflake/spark/snowflake/io/package.scala
@@ -1,6 +1,7 @@
 package net.snowflake.spark.snowflake
 
 import net.snowflake.spark.snowflake.Parameters.MergedParameters
+import net.snowflake.spark.snowflake.io.SupportedFormat.SupportedFormat
 import net.snowflake.spark.snowflake.io.SupportedSource.SupportedSource
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{SQLContext, SparkSession}
@@ -12,13 +13,14 @@ package object io {
                params: MergedParameters,
                sql: String,
                jdbcWrapper: JDBCWrapper = DefaultJDBCWrapper,
-               source: SupportedSource = SupportedSource.S3INTERNAL
+               source: SupportedSource = SupportedSource.S3INTERNAL,
+               format: SupportedFormat = SupportedFormat.CSV
              ): RDD[String] =
   {
 
     source match {
       case SupportedSource.S3INTERNAL =>
-        new S3InternalRDD(sqlContext, params, sql)
+        new S3InternalRDD(sqlContext, params, sql, jdbcWrapper)
       case SupportedSource.S3EXTERNAL =>
         null
     }
