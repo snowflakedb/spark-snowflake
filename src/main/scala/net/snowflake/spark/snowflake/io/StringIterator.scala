8a96c61903f45adf02726b534a552352a84fd6ae
Bing Li <binglihub@gmail.com>
2018-05-17 17:34:11 -0700

    fix bugs

--- src/main/scala/net/snowflake/spark/snowflake/io/StringIterator.scala
+++ src/main/scala/net/snowflake/spark/snowflake/io/StringIterator.scala
@@ -1,8 +1,11 @@
 package net.snowflake.spark.snowflake.io
 
 import java.io.InputStream
+import java.nio.charset.Charset
 
 import net.snowflake.spark.snowflake.io.SupportedFormat.SupportedFormat
+
+import scala.collection.mutable.ArrayBuffer
 
 
 private[io] class StringIterator(
@@ -10,59 +13,50 @@ private[io] class StringIterator(
                                 ) extends Iterator[String] {
 
   private val lineFeed: Byte = '\n'
+  private val quoteChar: Byte = '"'
 
   private var inputStreams: List[InputStream] = Nil
-  private var currentStream: InputStream = _
-  private var currentChar: Option[Char] = None
+  private var currentStream: Option[InputStream] = None
+  private var currentChar: Option[Byte] = None
 
   def addStream(stream: InputStream): Unit =
     inputStreams = stream :: Nil
 
-  override def hasNext: Boolean = !(inputStreams.isEmpty && currentStream == null)
+  override def hasNext: Boolean = inputStreams.nonEmpty || currentStream.isDefined
 
   override def next(): String = {
     if (!hasNext) null
     else {
-      if (currentStream == null) {
-        currentStream = inputStreams.head
+      if (currentStream.isEmpty) {
+        currentStream = Some(inputStreams.head)
         inputStreams = inputStreams.tail
         currentChar = None
       }
-      val buff = new StringBuilder()
+      val buff = ArrayBuffer.empty[Byte]
 
       format match {
         case SupportedFormat.CSV =>
-          var quotedNum = 0
           if (currentChar.isEmpty) currentChar = nextChar()
-          while (currentChar.isDefined && !(quotedNum % 2 == 0 && currentChar.get == lineFeed)) {
-            currentChar match {
-              case Some('\\') =>
-                buff.append(currentChar.get)
-                currentChar = nextChar()
-              case Some('"') =>
-                quotedNum += 1
-              case _ =>
-            }
 
+          while (currentChar.isDefined && currentChar.get != lineFeed) {
             buff.append(currentChar.get)
             currentChar = nextChar()
           }
           currentChar = nextChar()
           if (currentChar.isEmpty) {
-            currentStream.close()
-            currentStream = null
+            currentStream.foreach(_.close())
+            currentStream = None
           }
         case SupportedFormat.JSON =>
         //todo
       }
-
-      buff.toString()
+      new String(buff.toArray, Charset.forName("UTF-8"))
     }
   }
 
-  private def nextChar(): Option[Char] = {
-    val c = currentStream.read()
-    if (c < 0) None else Some(c.toChar)
+  private def nextChar(): Option[Byte] = {
+    val c = currentStream.get.read()
+    if (c < 0) None else Some(c.toByte)
   }
 
   def closeAll(): Unit =
