6e74db9b69093daebc872d561ea915c5d4906114
Bing Li <binglihub@gmail.com>
2018-05-21 15:43:50 -0700

    s3 internal write

--- src/main/scala/net/snowflake/spark/snowflake/io/package.scala
+++ src/main/scala/net/snowflake/spark/snowflake/io/package.scala
@@ -4,7 +4,8 @@ import net.snowflake.spark.snowflake.Parameters.MergedParameters
 import net.snowflake.spark.snowflake.io.SupportedFormat.SupportedFormat
 import net.snowflake.spark.snowflake.io.SupportedSource.SupportedSource
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{SQLContext, SparkSession}
+import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.{SQLContext, SaveMode, SparkSession}
 
 package object io {
 
@@ -16,26 +17,35 @@ package object io {
                source: SupportedSource = SupportedSource.S3INTERNAL,
                format: SupportedFormat = SupportedFormat.CSV
              ): RDD[String] =
-  {
-
     source match {
       case SupportedSource.S3INTERNAL =>
         new S3InternalRDD(sqlContext, params, sql, jdbcWrapper, format)
       case SupportedSource.S3EXTERNAL =>
         new S3External(sqlContext, params, sql, jdbcWrapper, format).getRDD()
     }
-  }
+
 
   def writeRDD(
-                session: SparkSession,
+                sqlContext: SQLContext,
                 params: MergedParameters,
                 rdd: RDD[String],
+                schema: StructType,
+                saveMode: SaveMode,
                 mapper: Option[Map[String, String]] = None,
                 jdbcWrapper: JDBCWrapper = DefaultJDBCWrapper,
                 source: SupportedSource = SupportedSource.S3INTERNAL
-              ): Boolean =
-  {
-    false
-  }
+              ): Unit =
+    source match {
+      case SupportedSource.S3INTERNAL =>
+        S3Writer.writeToS3Internal(
+          rdd,
+          schema,
+          sqlContext,
+          saveMode,
+          params,
+          jdbcWrapper,
+          SupportedSource.S3INTERNAL)
+      case SupportedSource.S3EXTERNAL =>
+    }
 
 }
