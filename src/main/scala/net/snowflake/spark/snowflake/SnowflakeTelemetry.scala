c2233f39c6ce0c145f49433932922de218722b18
Bing Li <binglihub@gmail.com>
2019-06-03 14:00:35 -0700

    rollback to Spark 2.2

--- src/main/scala/net/snowflake/spark/snowflake/SnowflakeTelemetry.scala
+++ src/main/scala/net/snowflake/spark/snowflake/SnowflakeTelemetry.scala
@@ -85,7 +85,7 @@ object SnowflakeTelemetry {
     val children = mapper.createArrayNode()
 
     plan match {
-      case LogicalRelation(sfRelation: SnowflakeRelation, _, _, _) =>
+      case LogicalRelation(sfRelation: SnowflakeRelation, _, _) =>
         isSFPlan = true
         action = "SnowflakeRelation"
         val schema = mapper.createArrayNode()
