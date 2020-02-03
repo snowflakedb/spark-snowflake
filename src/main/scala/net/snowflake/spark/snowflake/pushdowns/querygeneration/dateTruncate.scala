3598b1a333c79c3ada5851e33728a605b9f8f840
Josh Chacona <jjchacona@gmail.com>
2020-02-03 15:09:28 -0800

    Added support for date_trunc

---
+++ src/main/scala/net/snowflake/spark/snowflake/pushdowns/querygeneration/dateTruncate.scala
@@ -0,0 +1,43 @@
+package net.snowflake.spark.snowflake.pushdowns.querygeneration
+
+import net.snowflake.spark.snowflake.{ConstantString, SnowflakeSQLStatement}
+import org.apache.spark.sql.catalyst.expressions.{
+  Attribute,
+  date_trunc,
+  Expression,
+  Month,
+  Quarter,
+  Year,
+  Day,
+  Minute,
+  Second,
+}
+
+/** Extractor for boolean expressions (return true or false). */
+private[querygeneration] object dateTruncate {
+  def unapply(
+    expAttr: (Expression, Seq[Attribute])
+  ): Option[SnowflakeSQLStatement] = {
+    val expr = expAttr._1
+    val fields = expAttr._2
+
+    expr  = expr match {
+      case _: YYYY | _: YY => Year
+      case _: MON | _: MM => Month
+      case _: DAY | _: DD => Day
+      case _ => expr
+    }
+
+    Option(expr match {
+      case date_trunc(dateType, timeStamp) =>
+        ConstantString("DATE_TRUNC(" + convertStatement(dateType, fields) + "," +
+          convertStatement(timeStamp, fields) + ")"
+
+      case _: Month | _: Quarter | _: Year | _: HOUR | _: MINUTE | _: SECOND | _: WEEK =>
+        ConstantString(expr.prettyName.toUpperCase) +
+          blockStatement(convertStatements(fields, expr.children: _*))
+
+      case _ => null
+    })
+  }
+}
