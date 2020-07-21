309821411ebb8c8a9dd6b2629213bf3fcb4026ec
Mingli Rui <mingli.rui@snowflake.com>
2020-07-20 17:27:55 -0700

    SNOW-175813 Pushdown coalesce() to snowflake.

--- src/main/scala/net/snowflake/spark/snowflake/pushdowns/querygeneration/MiscStatement.scala
+++ src/main/scala/net/snowflake/spark/snowflake/pushdowns/querygeneration/MiscStatement.scala
@@ -14,6 +14,7 @@ import org.apache.spark.sql.catalyst.expressions.{
   Attribute,
   CaseWhen,
   Cast,
+  Coalesce,
   DenseRank,
   Descending,
   Expression,
@@ -135,6 +136,14 @@ private[querygeneration] object MiscStatement {
           case None => EmptySnowflakeSQLStatement()
         }} + ConstantString("END")
 
+      case Coalesce(columns) =>
+        ConstantString(expr.prettyName.toUpperCase) +
+          blockStatement(
+            mkStatement(
+              columns.map(convertStatement(_, fields)),
+              ", "
+            )
+          )
 
       case _ => null
     })
