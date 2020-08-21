5b3c432d61f2e5a0d9a54bf10c2b9fbabc9d818e
Mingli Rui <mingli.rui@snowflake.com>
2020-08-21 13:35:44 -0700

    SNOW-185844 Remove unused function for spark streaming and update test

--- src/main/scala/net/snowflake/spark/snowflake/streaming/SnowflakeIngestConnector.scala
+++ src/main/scala/net/snowflake/spark/snowflake/streaming/SnowflakeIngestConnector.scala
@@ -16,42 +16,6 @@ import scala.concurrent.duration._
 import scala.concurrent.{Await, Future, TimeoutException}
 
 object SnowflakeIngestConnector {
-
-  /**
-    * Check ingest load history, return a list of all failed file name
-    *
-    * @param files     a list of files being ingested
-    * @param frequency frequency of load history requests
-    * @param manager   ingest manager instance
-    * @return a list of failed file name
-    */
-  def waitForFileHistory(files: List[String], frequency: Long = 5000)(
-    implicit manager: SimpleIngestManager
-  ): List[String] = {
-    var checkList: Set[String] = files.toSet[String]
-    var beginMark: String = null
-    var failedFiles: List[String] = Nil
-
-    while (checkList.nonEmpty) {
-      Thread.sleep(frequency)
-      val response = manager.getHistory(null, null, beginMark)
-      beginMark = Option[String](response.getNextBeginMark).getOrElse(beginMark)
-      if (response != null && response.files != null) {
-        response.files.toList.foreach(
-          entry =>
-            if (entry.getPath != null && entry.isComplete && checkList
-                  .contains(entry.getPath)) {
-              checkList -= entry.getPath
-              if (entry.getStatus != IngestStatus.LOADED) {
-                failedFiles = entry.getPath :: failedFiles
-              }
-            }
-        )
-      }
-    }
-    failedFiles
-  }
-
   /**
     * list of (fileName, loading Succeed or not
     */
@@ -106,31 +70,6 @@ object SnowflakeIngestConnector {
                           port: Int = 443,
                           scheme: String = "https"): SimpleIngestManager =
     new SimpleIngestManager(account, user, pipe, privateKey, scheme, host, port)
-
-  /**
-    * Ingest files and wait ingest history
-    *
-    * @param files   a list of file names
-    * @param sec     time out in second
-    * @param manager Ingest Manager
-    * @return a list of failed file name, or None in case of time out
-    */
-  def ingestFilesAndCheck(files: List[String], sec: Long)(
-    implicit manager: SimpleIngestManager
-  ): Option[List[String]] = {
-    ingestFiles(files)
-
-    lazy val checker = Future {
-      waitForFileHistory(files)
-    }
-
-    try {
-      Some(Await.result(checker, sec second))
-    } catch {
-      case _: TimeoutException => None
-    }
-
-  }
 
   def ingestFiles(
     files: List[String]
