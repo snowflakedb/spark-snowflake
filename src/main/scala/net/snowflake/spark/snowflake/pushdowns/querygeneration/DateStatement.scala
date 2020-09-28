6a79a67e46cffdbaecdec372864cc098083e26c6
Mingli Rui <mingli.rui@snowflake.com>
2020-09-28 15:24:13 -0700

    Revert "SNOW-179329 Support pushdown for DateSub and AddMonths"
    This reverts commit 39faacdca80904d2dcb7bac152a7238d71469354.

--- src/main/scala/net/snowflake/spark/snowflake/pushdowns/querygeneration/DateStatement.scala
+++ src/main/scala/net/snowflake/spark/snowflake/pushdowns/querygeneration/DateStatement.scala
@@ -2,10 +2,8 @@ package net.snowflake.spark.snowflake.pushdowns.querygeneration
 
 import net.snowflake.spark.snowflake.{ConstantString, SnowflakeSQLStatement}
 import org.apache.spark.sql.catalyst.expressions.{
-  AddMonths,
   Attribute,
   DateAdd,
-  DateSub,
   Expression,
   Month,
   Quarter,
@@ -16,11 +14,6 @@ import org.apache.spark.sql.catalyst.expressions.{
 
 /** Extractor for boolean expressions (return true or false). */
 private[querygeneration] object DateStatement {
-  // DateAdd's pretty name in Spark is "date_add",
-  // the counterpart's name in SF is "DATEADD".
-  // And the syntax is some different.
-  val SNOWFLAKE_DATEADD = "DATEADD"
-
   def unapply(
     expAttr: (Expression, Seq[Attribute])
   ): Option[SnowflakeSQLStatement] = {
@@ -29,27 +22,8 @@ private[querygeneration] object DateStatement {
 
     Option(expr match {
       case DateAdd(startDate, days) =>
-        ConstantString(SNOWFLAKE_DATEADD) +
-          blockStatement(
-            ConstantString("day,") +
-              convertStatement(days, fields) + "," +
-              convertStatement(startDate, fields)
-          )
-
-      // Snowflake has no direct DateSub function,
-      // it is pushdown by DATEADD with negative days
-      case DateSub(startDate, days) =>
-        ConstantString(SNOWFLAKE_DATEADD) +
-          blockStatement(
-            ConstantString("day, (0 - (") +
-              convertStatement(days, fields) + ") )," +
-              convertStatement(startDate, fields)
-          )
-
-      case AddMonths(startDate, days) =>
-        ConstantString(expr.prettyName.toUpperCase) +
-          blockStatement(convertStatement(startDate, fields) + "," +
-            convertStatement(days, fields))
+        ConstantString("DATEADD(day,") + convertStatement(days, fields) + "," +
+          convertStatement(startDate, fields) + ")"
 
       case _: Month | _: Quarter | _: Year |
            _: TruncDate | _: TruncTimestamp =>
