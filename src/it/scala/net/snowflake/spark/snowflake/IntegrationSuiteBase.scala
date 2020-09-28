4b68fdb85ff308d225906f95dd882c7bb9fd6257
Mingli Rui <mingli.rui@snowflake.com>
2020-09-28 15:12:55 -0700

    Revert "SNOW-180368 Support pushdown for unix_timestamp()"
    This reverts commit bc15f27236eb0b5d70d357d20dbab435c647170c.

--- src/it/scala/net/snowflake/spark/snowflake/IntegrationSuiteBase.scala
+++ src/it/scala/net/snowflake/spark/snowflake/IntegrationSuiteBase.scala
@@ -228,10 +228,6 @@ trait IntegrationSuiteBase
       .master("local")
       .appName("SnowflakeSourceSuite")
       .config("spark.sql.shuffle.partitions", "6")
-      // "spark.sql.legacy.timeParserPolicy = LEGACY" is added to allow
-      // spark 3.0 to support legacy conversion for unix_timestamp().
-      // It may not be necessary for spark 2.X.
-      .config("spark.sql.legacy.timeParserPolicy", "LEGACY")
       .getOrCreate()
   }
 
--- src/main/scala/net/snowflake/spark/snowflake/pushdowns/querygeneration/DateStatement.scala
+++ src/main/scala/net/snowflake/spark/snowflake/pushdowns/querygeneration/DateStatement.scala
@@ -1,8 +1,18 @@
 package net.snowflake.spark.snowflake.pushdowns.querygeneration
 
 import net.snowflake.spark.snowflake.{ConstantString, SnowflakeSQLStatement}
-import org.apache.spark.sql.catalyst.expressions._
-import org.apache.spark.sql.types._
+import org.apache.spark.sql.catalyst.expressions.{
+  AddMonths,
+  Attribute,
+  DateAdd,
+  DateSub,
+  Expression,
+  Month,
+  Quarter,
+  TruncDate,
+  TruncTimestamp,
+  Year
+}
 
 /** Extractor for boolean expressions (return true or false). */
 private[querygeneration] object DateStatement {
@@ -45,42 +55,6 @@ private[querygeneration] object DateStatement {
            _: TruncDate | _: TruncTimestamp =>
         ConstantString(expr.prettyName.toUpperCase) +
           blockStatement(convertStatements(fields, expr.children: _*))
-
-      case UnixTimestamp(timeExp, format, _) =>
-        // There is no direct equivalent function for Spark unix_timestamp()
-        // in Snowflake. But, the equivalent functionality can be achieved by
-        // DATE_PART( <date_or_time_part> , <date_or_time_expr> ).
-        // Some highlights to using DATE_PART():
-        // 1. Spark unix_timestamp() is used to convert timestamp in seconds.
-        //    So Snowflake needs to use 'EPOCH_SECOND' for <date_or_time_part>.
-        // 2. Spark unix_timestamp() supports column type: Date, Timestamp
-        //    and String. SF DATE_PART() only supports the column type: Date
-        //    and Timestamp.
-        //    a) If datatype is DateType or TimestampType, we can use
-        //       "DATE_PART('EPOCH_SECOND', <Col>)" directly.
-        //    b) If datatype is String, Spark requires customer to provide
-        //       format (the default format is "yyyy-MM-dd HH:mm:ss").
-        //       So, we can use DATE_PART() + TO_TIMESTAMP(). For example,
-        //       "DATE_PART('EPOCH_SECOND', TO_TIMESTAMP(<Col>, format))"
-        // 3. Spark also supports unix_timestamp() to get current timestamp.
-        //    For this case, Spark has calculated it as a CONST value.
-        //    Pushdown doesn't need to care about it.
-        timeExp.dataType match {
-          case _: DateType | _: TimestampType =>
-            ConstantString("DATE_PART('EPOCH_SECOND',") +
-              blockStatement(convertStatement(timeExp, fields)) + ")"
-          case StringType =>
-            // Spark uses Java simpleDateFormat pattern for 'format'.
-            // https://docs.oracle.com/javase/tutorial/i18n/format/simpleDateFormat.html
-            // The format is some different to Snowflake's timestamp format.
-            // One different is that Spark uses 'mm' stand for minutes,
-            // but Snowflake uses 'MI', so it needs to be converted before
-            // passing to snowflake.
-            val sfFormat = format.toString().replaceAll("mm", "MI")
-            ConstantString("DATE_PART('EPOCH_SECOND', TO_TIMESTAMP") +
-              blockStatement(convertStatement(timeExp, fields) +
-                s",'$sfFormat'") + ")"
-        }
 
       case _ => null
     })
