fd077ff158b2c2792a0865b275f9c4dcd9296d7d
Edward Ma <63481447+sfc-gh-ema@users.noreply.github.com>
2020-10-07 18:28:42 -0700

    SNOW-201485: Port over Snowpark-style integration with Spark Datafram… (#297)
    * SNOW-201485: Port over Snowpark-style integration with Spark Dataframe Suites
    
    * Remove trailing comma in build file
    
    * add missing library import
    
    * Review comments
    
    * Replace QueryTest fail() with scalatest fail() for compatibility with older Spark versions
    
    * More comments
    
    * Reverting changes to Util files
    
    Co-authored-by: Edward Ma <etduwx@gmail.com>

---
+++ src/it/scala/org/apache/spark/sql/snowflake/SFTestSessionBase.scala
@@ -0,0 +1,66 @@
+package org.apache.spark.sql.snowflake
+
+import net.snowflake.spark.snowflake.{IntegrationEnv, Parameters, TestUtils}
+import org.apache.spark.sql.{SQLContext, SQLImplicits, SparkSession}
+
+import scala.util.Random
+
+trait SFTestSessionBase extends IntegrationEnv {
+  private var _spark: SFTestWrapperSparkSession = null
+  private var tempSchema: String = _
+  private var optionsTestTempSchema: Map[String, String] = _
+
+  def getSnowflakeSession(): SFTestWrapperSparkSession = {
+    initializeSession()
+    _spark
+  }
+
+  private def initializeSession(): Unit = {
+    if (_spark == null) {
+      _spark = createSession
+      SparkSession.setDefaultSession(_spark)
+      SparkSession.setActiveSession(_spark)
+    }
+  }
+
+  def createSession(): SFTestWrapperSparkSession = {
+    System.setProperty("hadoop.home.dir", "/tmp/hadoop")
+    SFTestWrapperSparkSession(sc, optionsTestTempSchema)
+  }
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+    // connectorOptionsTestTempSchema should have the temp schema replacing
+    // sfSchema
+    tempSchema = s"testTempSchema_${Math.abs(Random.nextLong()).toString}"
+    val optionsWithoutSchema = collection.mutable.Map() ++
+      connectorOptionsNoTable.filterKeys(_ != Parameters.PARAM_SF_SCHEMA)
+
+    optionsWithoutSchema.put(Parameters.PARAM_SF_SCHEMA, tempSchema)
+    optionsTestTempSchema = optionsWithoutSchema.toMap
+
+    jdbcUpdate(s"create or replace schema $tempSchema")
+    initializeSession()
+  }
+
+  override def afterAll(): Unit = {
+    try {
+      if (_spark != null) {
+        try {
+          _spark.sessionState.catalog.reset()
+        } finally {
+          _spark.stop()
+          _spark = null
+        }
+      }
+    } finally {
+      jdbcUpdate(s"drop schema if exists $tempSchema")
+      SparkSession.clearActiveSession()
+      SparkSession.clearDefaultSession()
+    }
+  }
+
+  protected object testSQLImplicits extends SQLImplicits {
+    protected override def _sqlContext: SQLContext = _spark.sqlContext
+  }
+}
---
+++ src/it/scala/org/apache/spark/sql/snowflake/SFTestWrapperSparkSession.scala
@@ -0,0 +1,39 @@
+package org.apache.spark.sql.snowflake
+
+import net.snowflake.spark.snowflake.{TestUtils, Utils}
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+import scala.reflect.runtime.universe.TypeTag
+import scala.util.Random
+
+class SFTestWrapperSparkSession(sc: SparkContext, sfConfigs: Map[String, String])
+    extends SparkSession(sc) {
+
+  override def createDataFrame[A <: Product: TypeTag](data: Seq[A]): DataFrame = {
+    // Create a DataFrame from the Seq using Spark's normal way.
+    val origDF = super.createDataFrame(data)
+
+    // Write this DataFrame to Snowflake, in effect testing the connector's write-out
+    // code path, and then read it back as a Snowflake DataFrame. This uses
+    // the temporary schema defined in the run properties, tempTestSchema
+    val tableName = s"tempTestTable_${Math.abs(Random.nextLong()).toString}"
+
+    origDF.write
+      .format(Utils.SNOWFLAKE_SOURCE_SHORT_NAME)
+      .options(sfConfigs)
+      .option("dbtable", tableName)
+      .save()
+    read
+      .format(Utils.SNOWFLAKE_SOURCE_SHORT_NAME)
+      .options(sfConfigs)
+      .option("dbtable", tableName)
+      .load()
+  }
+}
+
+object SFTestWrapperSparkSession {
+
+  def apply(context: SparkContext, configs: Map[String, String]): SFTestWrapperSparkSession =
+    new SFTestWrapperSparkSession(context, configs)
+}
