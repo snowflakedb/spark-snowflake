521a1dc4bd1c1a08a3a6a1d584ea363005ede151
Edward Ma <etduwx@gmail.com>
2020-10-26 17:17:28 -0700

    Change variable name

--- ClusterTest/src/main/scala/net/snowflake/spark/snowflake/PerfTest.scala
+++ ClusterTest/src/main/scala/net/snowflake/spark/snowflake/PerfTest.scala
@@ -4,8 +4,8 @@ import net.snowflake.spark.snowflake.benchmarks.{PerformanceSuite, TPCDSSuite}
 
 object PerfTest {
   def main(args: Array[String]): Unit = {
-    val columnMappingSuite: org.scalatest.FunSuite = new TPCDSSuite
+    val TPCDSSuite: org.scalatest.FunSuite = new TPCDSSuite
 
-    columnMappingSuite.execute()
+    TPCDSSuite.execute()
   }
 }
