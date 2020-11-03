1b80a4c9e557a5e0188a2e40152ffe9103c094b8
Edward Ma <etduwx@gmail.com>
2020-11-02 17:22:26 -0800

    script

--- ClusterTest/run_stress_test.sh
+++ ClusterTest/run_stress_test.sh
@@ -1,38 +1,19 @@
 #!/bin/bash
 
-echo "This script run inside of the docker container."
+export TEST_COMPILE_SCALA_VERSION=2.12
+export TEST_SPARK_CONNECTOR_VERSION=2.8.2
+export TEST_SPARK_VERSION=3.0
+export TEST_CLUSTERTEST_VERSION=1.0
 
-echo "Below variables must be set by workflow and docker-conpose file"
-echo "GITHUB_RUN_ID=$GITHUB_RUN_ID"
-echo "GITHUB_SHA=$GITHUB_SHA"
-echo "TEST_SPARK_CONNECTOR_VERSION=$TEST_SPARK_CONNECTOR_VERSION"
-echo "TEST_SCALA_VERSION=$TEST_SCALA_VERSION"
-echo "TEST_COMPILE_SCALA_VERSION=$TEST_COMPILE_SCALA_VERSION"
-echo "TEST_JDBC_VERSION=$TEST_JDBC_VERSION"
-echo "TEST_SPARK_VERSION=$TEST_SPARK_VERSION"
-echo "SNOWFLAKE_TEST_CONFIG=$SNOWFLAKE_TEST_CONFIG"
+# Test Revision ID
+export TEST_REVISION_ID=0
 
-export SPARK_HOME=/users/spark
-export SPARK_WORKDIR=/users/spark/work
+export SPARK_CONNECTOR_JAR_PATH=${SF_CONNECTOR_DIR}/target/scala-${TEST_COMPILE_SCALA_VERSION}/spark-snowflake-assembly-${TEST_SPARK_CONNECTOR_VERSION}-spark_${TEST_SPARK_VERSION}.jar
+export CLUSTERTEST_JAR_PATH=${SF_CONNECTOR_DIR}/ClusterTest/target/scala-${TEST_COMPILE_SCALA_VERSION}/ClusterTest-assembly-${TEST_CLUSTERTEST_VERSION}.jar
 
-export SPARK_CONNECTOR_JAR_NAME=spark-snowflake_${TEST_SCALA_VERSION}-${TEST_SPARK_CONNECTOR_VERSION}-spark_${TEST_SPARK_VERSION}.jar
-export JDBC_JAR_NAME=snowflake-jdbc-${TEST_JDBC_VERSION}.jar
-
-# Check test file exists
-ls -al $SNOWFLAKE_TEST_CONFIG \
-       $SPARK_WORKDIR/${SPARK_CONNECTOR_JAR_NAME} \
-       $SPARK_WORKDIR/${JDBC_JAR_NAME} \
-       $SPARK_WORKDIR/clustertest_${TEST_SCALA_VERSION}-1.0.jar \
-       $SPARK_WORKDIR/ClusterTest.py
-
-echo "Important: if new test cases are added, script .github/docker/check_result.sh MUST be updated"
-
-spark-submit --jars /home/hadoop/spark-snowflake-jar_assembly/target/scala-2.12/spark-snowflake-assembly-2.8.2-spark_3.0.jar --class net.snowflake.spark.snowflake.ClusterTest   --master spark://localhost:7077   /home/hadoop/spark-snowflake-jar_assembly/ClusterTest/target/scala-2.12/ClusterTest-assembly-1.0.jar remote "net.snowflake.spark.snowflake.testsuite.StressReadWriteSuite;" 3
-
-$SPARK_HOME/bin/spark-submit \
-      --jars $SPARK_WORKDIR/${SPARK_CONNECTOR_JAR_NAME},$SPARK_WORKDIR/${JDBC_JAR_NAME} \
-      --conf "spark.executor.extraJavaOptions=-Djava.io.tmpdir=$SPARK_WORKDIR  -Dnet.snowflake.jdbc.loggerImpl=net.snowflake.client.log.SLF4JLogger -Dlog4j.configuration=file://${SPARK_HOME}/conf/log4j_executor.properties" \
-      --conf "spark.driver.extraJavaOptions=-Djava.io.tmpdir=$SPARK_WORKDIR -Dnet.snowflake.jdbc.loggerImpl=net.snowflake.client.log.SLF4JLogger -Dlog4j.configuration=file://${SPARK_HOME}/conf/log4j_driver.properties" \
-      --master spark://master:7077 --deploy-mode client \
+spark-submit \
+      --jars $SPARK_CONNECTOR_JAR_PATH \
+      --master yarn \
       --class net.snowflake.spark.snowflake.ClusterTest \
-      $SPARK_WORKDIR/clustertest_${TEST_SCALA_VERSION}-1.0.jar remote "net.snowflake.spark.snowflake.testsuite.BasicReadWriteSuite;" github
+      $CLUSTERTEST_JAR_PATH remote "net.snowflake.spark.snowflake.testsuite.StressReadWriteSuite;" stress $TEST_REVISION_ID
+
