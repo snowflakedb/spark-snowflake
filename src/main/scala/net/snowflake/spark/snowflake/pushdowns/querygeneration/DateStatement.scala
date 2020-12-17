26c476176dc451f216fa297b6983283ec72e3ca7
Mingli Rui <63472932+sfc-gh-mrui@users.noreply.github.com>
2020-12-17 13:04:19 -0800

    SNOW-179329 Support pushdown for DateSub (#323)
    * SNOW-179329 Support pushdown for DateSub
    
    * SNOW-179329 Remove the pushdown of AddMonths and add more tests

--- src/main/scala/net/snowflake/spark/snowflake/pushdowns/querygeneration/DateStatement.scala
+++ src/main/scala/net/snowflake/spark/snowflake/pushdowns/querygeneration/DateStatement.scala
@@ -2,8 +2,10 @@ package net.snowflake.spark.snowflake.pushdowns.querygeneration
 
 import net.snowflake.spark.snowflake.{ConstantString, SnowflakeSQLStatement}
 import org.apache.spark.sql.catalyst.expressions.{
+  AddMonths,
   Attribute,
   DateAdd,
+  DateSub,
   Expression,
   Month,
   Quarter,
@@ -14,6 +16,11 @@ import org.apache.spark.sql.catalyst.expressions.{
 
 /** Extractor for boolean expressions (return true or false). */
 private[querygeneration] object DateStatement {
+  // DateAdd's pretty name in Spark is "date_add",
+  // the counterpart's name in SF is "DATEADD".
+  // And the syntax is some different.
+  val SNOWFLAKE_DATEADD = "DATEADD"
+
   def unapply(
     expAttr: (Expression, Seq[Attribute])
   ): Option[SnowflakeSQLStatement] = {
@@ -22,8 +29,29 @@ private[querygeneration] object DateStatement {
 
     Option(expr match {
       case DateAdd(startDate, days) =>
-        ConstantString("DATEADD(day,") + convertStatement(days, fields) + "," +
-          convertStatement(startDate, fields) + ")"
+        ConstantString(SNOWFLAKE_DATEADD) +
+          blockStatement(
+            ConstantString("day,") +
+              convertStatement(days, fields) + "," +
+              convertStatement(startDate, fields)
+          )
+
+      // Snowflake has no direct DateSub function,
+      // it is pushdown by DATEADD with negative days
+      case DateSub(startDate, days) =>
+        ConstantString(SNOWFLAKE_DATEADD) +
+          blockStatement(
+            ConstantString("day, (0 - (") +
+              convertStatement(days, fields) + ") )," +
+              convertStatement(startDate, fields)
+          )
+
+      // AddMonths can't be pushdown to snowflake because their functionality is different.
+      // For Snowflake and Spark 2.3/2.4, AddMonths() will preserve the end-of-month information.
+      // But, Spark 3.0, it doesn't. For example,
+      // On spark 2.3/2.4, "2015-02-28" +1 month -> "2015-03-31"
+      // On spark 3.0,     "2015-02-28" +1 month -> "2015-03-28"
+      case AddMonths(_, _) => null
 
       case _: Month | _: Quarter | _: Year |
            _: TruncDate | _: TruncTimestamp =>
