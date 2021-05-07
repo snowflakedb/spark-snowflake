7c77eba5adaf02580336f2d3d7994d271c8a3711
sfc-gh-abhatnagar <ankit.bhatnagar@snowflake.com>
2021-05-07 15:47:59 -0700

    Update version as per review comments

--- src/main/scala/net/snowflake/spark/snowflake/Utils.scala
+++ src/main/scala/net/snowflake/spark/snowflake/Utils.scala
@@ -54,7 +54,7 @@ object Utils {
     */
   val SNOWFLAKE_SOURCE_SHORT_NAME = "snowflake"
 
-  val VERSION = "2.8.5"
+  val VERSION = "2.8.6"
 
   /**
     * The certified JDBC version to work with this spark connector version.
