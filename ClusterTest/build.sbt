df98ec617e4bb5192680dc8c6046c4a1032fa9a6
Mingli Rui <mingli.rui@snowflake.com>
2021-05-17 16:33:28 -0700

    SNOW-358401 Support spark 3.1

--- ClusterTest/build.sbt
+++ ClusterTest/build.sbt
@@ -16,8 +16,8 @@
 
 val sparkConnectorVersion = "2.8.5"
 val scalaVersionMajor = "2.12"
-val sparkVersionMajor = "3.0"
-val sparkVersion = s"${sparkVersionMajor}.0"
+val sparkVersionMajor = "3.1"
+val sparkVersion = s"${sparkVersionMajor}.1"
 val testSparkVersion = sys.props.get("spark.testVersion").getOrElse(sparkVersion)
 
 unmanagedJars in Compile += file(s"../target/scala-${scalaVersionMajor}/spark-snowflake_${scalaVersionMajor}-${sparkConnectorVersion}-spark_${sparkVersionMajor}.jar")
--- src/main/scala/net/snowflake/spark/snowflake/pushdowns/querygeneration/QueryBuilder.scala
+++ src/main/scala/net/snowflake/spark/snowflake/pushdowns/querygeneration/QueryBuilder.scala
@@ -193,8 +193,21 @@ private[querygeneration] class QueryBuilder(plan: LogicalPlan) {
           }
         }
 
-      case Union(children) =>
-        Some(UnionQuery(children, alias.next))
+      // From Spark 3.1, Union has 3 parameters
+      case Union(children, byName, allowMissingCol) =>
+        if (byName || allowMissingCol) {
+          // This exception is not a real issue. It will be caught in
+          // QueryBuilder.treeRoot and a telemetry message will be sent if
+          // there are any snowflake tables in the query.
+          throw new SnowflakePushdownUnsupportedException(
+            SnowflakeFailMessage.FAIL_PUSHDOWN_UNSUPPORTED_UNION,
+            s"${plan.nodeName} with byName=$byName allowMissingCol=$allowMissingCol",
+            plan.getClass.getName,
+            false
+          )
+        } else {
+          Some(UnionQuery(children, alias.next))
+        }
 
       case Expand(projections, output, child) =>
         val children = projections.map { p =>
