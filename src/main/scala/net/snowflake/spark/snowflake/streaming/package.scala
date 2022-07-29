f0ef7c23d788f9399595b31954b0f2af51388007
Mingli Rui <mingli.rui@snowflake.com>
2022-07-29 16:47:48 -0700

    SNOW-635353 Don't use parse_json() for COPY INTO with JSON file. When writing DF with complex type to a snowflake table, the COPY INTO TABLE command generated by Spark Connector uses "parse_json($1):<column_name>". This is inefficient. Spark Connector should use "$1:<column_name>".

--- src/main/scala/net/snowflake/spark/snowflake/streaming/package.scala
+++ src/main/scala/net/snowflake/spark/snowflake/streaming/package.scala
@@ -121,17 +121,18 @@ package object streaming {
                              from: String): String =
       format match {
         case SupportedFormat.JSON =>
+          val columnPrefix = if (param.useParseJsonForWrite) "parse_json($1):" else "$1:"
           if (list.isEmpty || list.get.isEmpty) {
             val names =
               tableSchema.fields
                 .map(
                   x =>
-                    "parse_json($1):".concat(Utils.quotedNameIgnoreCase(x.name))
+                    columnPrefix.concat(Utils.quotedNameIgnoreCase(x.name))
                 )
                 .mkString(",")
             s"from (select $names $from tmp)"
           } else {
-            s"from (select ${list.get.map(x => "parse_json($1):".concat(
+            s"from (select ${list.get.map(x => columnPrefix.concat(
               Utils.quotedNameIgnoreCase(x._2))).mkString(", ")} $from tmp)"
           }
         case SupportedFormat.CSV =>
