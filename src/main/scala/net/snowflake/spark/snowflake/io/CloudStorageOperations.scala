cd820b34a355680e7e3182a39aa63e0c17913e35
Wufan Shangguan <wufan.shangguan@snowflake.com>
2025-09-08 18:46:14 -0700

    clean code

--- src/main/scala/net/snowflake/spark/snowflake/io/CloudStorageOperations.scala
+++ src/main/scala/net/snowflake/spark/snowflake/io/CloudStorageOperations.scala
@@ -317,7 +317,6 @@ object CloudStorageOperations {
             }
             (creds.getAWSAccessKeyId, creds.getAWSSecretKey, token)
           case None =>
-            // TODO: fall back to creds.getAWSAccessKeyId, creds.getAWSSecretKey
             // Fall back to direct credential parameters
             (param.awsAccessKey.orNull, param.awsSecretKey.orNull, None)
         }
