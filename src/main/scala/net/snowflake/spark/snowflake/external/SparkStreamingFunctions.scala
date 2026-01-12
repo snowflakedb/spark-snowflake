885b621b93ea691536c2e3e791d45d362e823dc2
Wufan Shangguan <wufan.shangguan@snowflake.com>
2026-01-12 12:11:28 -0800

    spark 4 api change 2

--- src/main/scala/net/snowflake/spark/snowflake/external/SparkStreamingFunctions.scala
+++ src/main/scala/net/snowflake/spark/snowflake/external/SparkStreamingFunctions.scala
@@ -1,13 +1,21 @@
 package org.apache.spark.sql.snowflake
 
-import org.apache.spark.sql.{DataFrame, SQLContext}
+import org.apache.spark.sql.{DataFrame, Row, SQLContext}
+import org.apache.spark.sql.catalyst.CatalystTypeConverters
 
 object SparkStreamingFunctions {
 
   def streamingToNonStreaming(sqlContext: SQLContext,
-                              dataFrame: DataFrame): DataFrame =
-    sqlContext.sparkSession.createDataFrame(
-      dataFrame.queryExecution.toRdd,
-      dataFrame.schema
-    )
+                              dataFrame: DataFrame): DataFrame = {
+    val sparkSession = sqlContext.sparkSession
+    val schema = dataFrame.schema
+    
+    // Convert RDD[InternalRow] to RDD[Row] using CatalystTypeConverters
+    val converter = CatalystTypeConverters.createToScalaConverter(schema)
+    val rowRdd = dataFrame.queryExecution.toRdd.map { internalRow =>
+      converter(internalRow).asInstanceOf[Row]
+    }
+    
+    sparkSession.createDataFrame(rowRdd, schema)
+  }
 }
